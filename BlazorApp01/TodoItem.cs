3b10e4f69d83fcbf1fb0fbcd410c8cf458af1e3b
dev_kelo <clamp1981@gmail.com>
2023-01-27 11:38:05 +0900

    day 01, binding and event

---
+++ BlazorApp01/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorApp01
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+
+        public TodoItem(string? title, bool isDone)
+        {
+            Title = title;
+            IsDone = isDone;    
+        }
+    }
+}
